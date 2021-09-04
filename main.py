46af2ef2e6a91e05969696217f6f71e04a889eec
Keji0001 <90094133+Keji0001@users.noreply.github.com>
2021-09-04 15:38:04 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,230 @@
+#python 3.7.1
+
+# wifi hack software 2.0
+# the cleaner version of the orginal one
+# make sure to give me a star👑
+
+print("~~~~~~~~~WIFI HACKING SOFTWARE~~~~~~~~~~~")
+
+if input('TYPE "start" TO START THE HACKING PROCESS.\n> ') == 'start': 
+ import time
+time.sleep(1)
+print("CONNECTING TO SERVER")
+time.sleep(0.5)
+print("2%")
+time.sleep(0.5)
+print("8%")
+time.sleep(0.5)
+print("12%")
+time.sleep(0.5)
+print("19%")
+time.sleep(0.5)
+print("27%")
+time.sleep(1.5)
+print("43%")
+time.sleep(0.5)
+print("69%")
+time.sleep(0.5)
+print("75%")
+time.sleep(3)
+print("99%")
+print("CONNECTED TO THE SERVER")
+time.sleep(2)
+print("PROCESS STARTED!")
+print(".     ")
+time.sleep(1)
+name = input("WIFI NAME? -> ")
+print(".            ")
+
+import time 
+time.sleep(1)
+print("TESTING ALL POSSIBLE ENCRYPTED PASSWORDS COMBINATIONS ON",name)
+time.sleep(1)
+print("PASSWORD oCUIKuY6pFLFZWHSkks1 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 40U8c3LwIii1bxD0lJPP FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 2D2Jyn89mx82rCxAzOMQ FAILED! ")
+time.sleep(0.5)
+print("PASSWORD RrHG7tWcUTCCF7lqzO5S FAILED! ")
+time.sleep(0.5)
+print("PASSWORD SN96F6ezFnMYo1XbWN5a FAILED! ")
+time.sleep(0.5)
+print("PASSWORD wZULWrEM51NPKgoqyJ1P FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 8pFcob76aubu7gP5XfCx FAILED! ")
+time.sleep(0.5)
+print("PASSWORD HoFTWhxkKRbGIBW14efw FAILED! ")
+time.sleep(0.5)
+print("PASSWORD VzZRCHfSag2WiV3jZRfW FAILED! ")
+time.sleep(0.5)
+print("PASSWORD ZVY2qHPzq88k6sjYPJEm FAILED! ")
+time.sleep(0.5)
+print("PASSWORD rNlBX5Dj7rCVb74ZtK0y FAILED! ")
+time.sleep(0.5)
+print("PASSWORD JXiOS7iI28n8Ibr9tjgx FAILED! ")
+time.sleep(0.5)
+print("PASSWORD PcBiwW48Z2b038iEeQZi FAILED! ")
+time.sleep(0.5)
+print("PASSWORD CG2loPOnaICczYoBKsL1 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 2CBpYUiLWDQvKEgKy2Lk FAILED! ")
+time.sleep(0.5)
+print("PASSWORD CP7TE02dNhhgPVMtO82x FAILED! ")
+time.sleep(0.5)
+print("PASSWORD atMtC5teqC0SWnYO7DqT FAILED! ")
+time.sleep(0.5)
+print("PASSWORD V1SiKOAOaSasOAj0KeS0 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD H8iQuiItWEdXLLsvetgq FAILED! ")
+time.sleep(0.5)
+print("PASSWORD ugLaXJaaRGXYl79Q8lnn FAILED! ")
+time.sleep(0.5)
+print("PASSWORD cHNLfybtHsbqVJZM8iIl FAILED! ")
+time.sleep(0.5)
+print("PASSWORD QU4rLVlnqxzPDPRVcnxM FAILED! ")
+time.sleep(0.5)
+print("PASSWORD OppSiHx5s8fiElfHsUUF FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 5v9YA265AsnXF7lFHCKA FAILED! ")
+time.sleep(0.5)
+print("PASSWORD KIR59i1OIbmuM1NGmB8J FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 1HeMe5r15Se99Qw3kRPi FAILED! ")
+time.sleep(0.5)
+print("PASSWORD lcqWuIcMBZYqdHAWItFp FAILED! ")
+time.sleep(0.5)
+print("PASSWORD JDskep17daJ4wW2S2TmF FAILED! ")
+time.sleep(0.5)
+print("PASSWORD ZUEwPLgGhGGeWfh5IER6 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD sHyX64GVYVLKDMDdXOsO FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 5toULXL9TgLyDwyEerAO FAILED! ")
+time.sleep(0.5)
+print("PASSWORD pFXmcMUwvESlVQ5PXASo FAILED! ")
+time.sleep(0.5)
+print("PASSWORD FTz4tTJupOYY8HvFHHCf FAILED! ")
+time.sleep(0.5)
+print("PASSWORD u9p728wEj21p6QclkLf3 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD C0pyUTZb9c2ag43hBiVq FAILED! ")
+time.sleep(0.5)
+print("PASSWORD SBcu2PFi4lPDNJBhqxqH FAILED! ")
+time.sleep(0.5)
+print("PASSWORD tRlh43azXx0YfEcaYZLJ FAILED! ")
+time.sleep(0.5)
+print("PASSWORD q4jKdnotxh1GaBN2R5uB FAILED! ")
+time.sleep(0.5)
+print("PASSWORD tBSU6WdZquanXDoGRap6 FAILED! ")
+time.sleep(0.5)
+print("PASSWORD XIeyR7R1pggL8SOCtd5D FAILED! ")
+time.sleep(0.5)
+print("PASSWORD o7nBcKiACMXHEsKuOSMg FAILED! ")
+time.sleep(0.5)
+print("PASSWORD cbzR0uzkHTE9wDCBEUsN FAILED! ")
+time.sleep(0.5)
+print("PASSWORD btH2ebIRA5Lek0sp7wLQ FAILED! ")
+time.sleep(0.5)
+print("PASSWORD 0sp7wLQzR0uzkHXIeyUE FAILED! ")
+time.sleep(0.5)
+print("PASSWORD ******************** SUCCESS! ")
+time.sleep(2)
+print("                                                     ")
+print(" CORRECT ENCRYPTED PASSWORD FOUND! ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("                                                       ")
+
+
+
+import time
+time.sleep(1)
+print("UNENCRYPTING PASSWORD..... ")
+time.sleep(0.3)
+print("%1")
+time.sleep(0.3)
+print("%15")
+time.sleep(0.3)
+print("%20")
+time.sleep(0.3)
+print("%25")
+time.sleep(0.3)
+print("%30")
+time.sleep(0.3)
+print("%35")
+time.sleep(0.5)
+print("%47")
+time.sleep(0.4)
+print("%51")
+time.sleep(0.2)
+print("%60")
+time.sleep(0.9)
+print("%63")
+time.sleep(0.6)
+print("%78")
+time.sleep(0.3)
+print("%80")
+time.sleep(0.3)
+print("%85")
+time.sleep(0.1)
+print("%90")
+time.sleep(0.8)
+print("%97")
+time.sleep(0.7)
+print("%99")
+time.sleep(1)
+print("%100")
+
+print("PASSWORD FULLY UNENCRYPTED")
+
+
+
+
+time.sleep(2)
+import string
+from random import *
+
+letters = string.ascii_letters 
+digits = string.digits 
+
+chars = letters + digits
+
+min_len = 8
+max_len = 16
+password = "".join(choice(chars) for x in range(randint(min_len, max_len)))
+print("PASSWORD IS",password)
+
+time.sleep(1)
+
+import socket
+hostn = socket.gethostname()
+IPAD = socket.gethostbyname(hostn)
+print ("IP IS",IPAD)
+
+time.sleep(1)
+
+print ("Disclaimer: In case of an investigation")
+print ("by any federal entity or similar,I do not have")
+print ("any involvement with this group or with the")
+print ("people in it, I do not know how I am here,")
+print ("probably added by a third party, I do not") 
+print ("support any actions by the member of this group.") 
+
+
+      
